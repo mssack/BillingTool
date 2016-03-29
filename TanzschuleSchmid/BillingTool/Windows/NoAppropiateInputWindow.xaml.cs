5419182e19eef0db65760a61021556da9b547d10
Christian Sack (SURFACE) <christian@sack.at>
2016-03-29 15:31:15 +0200

    Added window ideas

---
+++ TanzschuleSchmid/BillingTool/Windows/NoAppropiateInputWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BillingTool.Windows
+{
+	/// <summary>
+	/// Interaction logic for NoAppropiateInputWindow.xaml
+	/// </summary>
+	public partial class NoAppropiateInputWindow : Window
+	{
+		public NoAppropiateInputWindow()
+		{
+			InitializeComponent();
+		}
+	}
+}
