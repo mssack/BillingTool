b4548533ad83f2899dc8f62e62573764a3be33c3
Christian Sack (SURFACE) <christian@sack.at>
2016-03-29 15:13:27 +0200

    First structure for startup params

--- TanzschuleSchmid/BillingTool/App.xaml.cs
+++ TanzschuleSchmid/BillingTool/App.xaml.cs
@@ -1,16 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
+﻿// Copyright (c) 2016 All rights reserved Christian Sack, Michael Sack
+// <author>Christian Sack</author>
+// <email>christian@sack.at</email>
+// <website>christian.sack.at</website>
+// <date>2016-03-29</date>
+
+using System;
 using System.Windows;
+
+
+
+
+
 
 namespace BillingTool
 {
-	/// <summary>
-	/// Interaction logic for App.xaml
-	/// </summary>
+	/// <summary>Interaction logic for App.xaml</summary>
 	public partial class App : Application
 	{
+		private void App_OnStartup(object sender, StartupEventArgs e)
+		{
+			//TODO use startup params to modify application startup.
+		}
 	}
 }
---
+++ TanzschuleSchmid/BillingTool/RuntimeSettings/StartupParams.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) 2016 All rights reserved Christian Sack, Michael Sack
+// <author>Christian Sack</author>
+// <email>christian@sack.at</email>
+// <website>christian.sack.at</website>
+// <date>2016-03-29</date>
+
+using System;
+
+
+
+
+
+
+namespace BillingTool.RuntimeSettings
+{
+	public sealed class StartupParams
+	{
+		private static StartupParams _instance;
+		private static readonly object SingletonLock = new object();
+
+		/// <summary>Returns the singleton instance</summary>
+		internal static StartupParams I
+		{
+			get
+			{
+				if (_instance != null)
+					return _instance; //Advanced first check to improve performance (no lock needed).
+				lock (SingletonLock)
+				{
+					return _instance ?? (_instance = new StartupParams());
+				}
+			}
+		}
+
+		private StartupParams()
+		{
+		}
+	}
+}
