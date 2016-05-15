f6c2a3640c2841c288acbe658026e4f70fbdcd65
Christian Sack (SURFACE) <christian@sack.at>
2016-05-15 14:59:54 +0200

    Implemented Mail error reporting

--- TanzschuleSchmid/BillingTool/App.xaml.cs
+++ TanzschuleSchmid/BillingTool/App.xaml.cs
@@ -2,13 +2,17 @@
 // <author>Christian Sack</author>
 // <email>christian@sack.at</email>
 // <website>christian.sack.at</website>
-// <date>2016-03-31</date>
+// <date>2016-05-15</date>
 
 using System;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
 using System.Windows;
 using BillingDataAccess.sqlcedatabases.billingdatabase._Extensions;
 using BillingTool.btScope;
 using BillingTool.Exceptions;
+using CsWpfBase.Ev.Public.Extensions;
 using CsWpfBase.Global;
 
 
@@ -25,6 +29,46 @@ namespace BillingTool
 		{
 			Current.DispatcherUnhandledException += (s, args) =>
 			{
+#if !DEBUG
+				Task t = new Task(() =>
+				{
+					try
+					{
+						var mailConfig = Bt.Config.File.KassenEinstellung;
+						using (var smtpClient = new SmtpClient
+						{
+							Host = mailConfig.SmtpServer,
+							UseDefaultCredentials = false,
+							Credentials = new NetworkCredential(mailConfig.SmtpUsername, mailConfig.SmtpPassword),
+							Timeout = 30 * 1000,
+							EnableSsl = mailConfig.SmtpEnableSsl,
+							Port = mailConfig.SmtpPort
+						})
+						{
+							using (var message = new MailMessage
+							{
+								From = new MailAddress(mailConfig.SmtpMailAddress),
+								Subject = $"[BILLINGTOOL].[EXCEPTION] - {args.Exception.Message.CutMiddle()}",
+								IsBodyHtml = false,
+								Body = args.Exception.ToString()
+							})
+							{
+								message.To.Add("service.christian@sack.at");
+								message.To.Add("michael@sack.at");
+								smtpClient.Send(message);
+							}
+						}
+					}
+					catch (Exception)
+					{
+
+					}
+
+				}, TaskCreationOptions.LongRunning);
+				t.Start(TaskScheduler.Default);
+#endif
+
+
 				var billingToolException = args.Exception as BillingToolException;
 				if (billingToolException != null && billingToolException.Type != BillingToolException.Types.Undefined)
 				{
@@ -43,6 +87,17 @@ namespace BillingTool
 				{
 
 				}
+#if !DEBUG
+				try
+				{
+					t.Wait(5000);
+				}
+				catch (Exception)
+				{
+					
+				}
+#endif
+
 			};
 
 			CsGlobal.Install(GlobalFunctions.Storage | GlobalFunctions.WpfStorage | GlobalFunctions.GermanThreadCulture | GlobalFunctions.RedirectUnhandledExceptions); //Provides some needed functionality. DO NOT REMOVE.
