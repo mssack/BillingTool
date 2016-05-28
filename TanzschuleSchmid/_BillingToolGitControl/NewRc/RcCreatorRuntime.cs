14499fa77914461b77967ccaff9528006d9daa50
Christian Sack (SURFACE) <christian@sack.at>
2016-05-28 19:18:58 +0200

    merged

---
+++ TanzschuleSchmid/_BillingToolGitControl/NewRc/RcCreatorRuntime.cs
@@ -0,0 +1,79 @@
+// Copyright (c) 2016 All rights reserved Christian Sack, Michael Sack
+// <author>Christian Sack</author>
+// <email>christian@sack.at</email>
+// <website>christian.sack.at</website>
+// <date>2016-05-28</date>
+
+using System;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using CsWpfBase.Ev.Public.Extensions;
+
+
+
+
+
+
+namespace BillingToolGitControl.NewRc
+{
+	public class RcCreatorRuntime
+	{
+		private readonly string _messageList;
+
+		public RcCreatorRuntime(string messageList)
+		{
+			_messageList = messageList;
+		}
+
+
+		public void Run()
+		{
+			DeleteAllActualFolders();
+
+			Utils.CreateTestEnvironment(Utils.Paths.Arc.Folder);
+
+			Zipping();
+			ChangeAnhängeReadme();
+			ChangeStartseiteReadme();
+
+			CommitViaCommandline();
+		}
+
+		private void DeleteAllActualFolders()
+		{
+			new DirectoryInfo(Utils.Paths.Destination.RcFolder).GetDirectories("RC*").ForEach(di => di.Delete(true));
+		}
+		
+		private void Zipping()
+		{
+			new FileInfo(Utils.Paths.Destination.ZipFile).DeleteFile_IfExists();
+			ZipFile.CreateFromDirectory(Utils.Paths.Arc.Folder, Utils.Paths.Destination.ZipFile, CompressionLevel.Optimal, false, Encoding.UTF8);
+		}
+
+		private void ChangeAnhängeReadme()
+		{
+			var txtLines = File.ReadAllLines(Utils.Paths.Source.AnhängeReadmeFile).ToList(); //Fill a list with the lines from the text file.
+			txtLines.Insert(txtLines.IndexOf("####Release Candidates") + 1, $"* [{Utils.BuildDetails.NameWithDate}](_ReleaseCandidates/{Utils.Paths.Destination.ZipFileName}?raw=true)" +
+																			$" (Computer: {Utils.BuildDetails.Machine}, User: {Utils.BuildDetails.User})"
+																			+ (string.IsNullOrEmpty(_messageList) ? "" : "\n\t* " + Regex.Split(_messageList.Replace("\r\n", "\n"), "\n").Join("\n\t* ")));
+			File.WriteAllLines(Utils.Paths.Source.AnhängeReadmeFile, txtLines);
+		}
+
+		private void ChangeStartseiteReadme()
+		{
+			var txtLines = File.ReadAllLines(Utils.Paths.Source.StartseiteReadmeFile).ToList(); //Fill a list with the lines from the text file.
+			txtLines[txtLines.FindIndex(x=>x.StartsWith("<!--AktuelleVersion-->"))] = $"<!--AktuelleVersion-->Aktuell [{Utils.BuildDetails.NameWithDate}](TanzschuleSchmid/_Anhänge/_ReleaseCandidates/{Utils.Paths.Destination.ZipFileName}?raw=true).";
+			File.WriteAllLines(Utils.Paths.Source.StartseiteReadmeFile, txtLines);
+		}
+
+		private void CommitViaCommandline()
+		{
+			Utils.CommitFiles($"New Release Candidate {Utils.BuildDetails.Name}", Utils.Paths.Source.BuildDetails, Utils.Paths.Source.AnhängeReadmeFile, Utils.Paths.Source.StartseiteReadmeFile, Utils.Paths.Destination.ZipFile);
+		}
+
+
+	}
+}
