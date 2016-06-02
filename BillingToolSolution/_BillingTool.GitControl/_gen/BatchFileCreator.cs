a38da06bdcc6c492a31a4b03907dff4ee6bc926f
Christian Sack (SURFACE) <christian@sack.at>
2016-06-02 10:57:41 +0200

    New Batch files implemented

--- BillingToolSolution/_BillingTool.GitControl/_gen/BatchFileCreator.cs
+++ BillingToolSolution/_BillingTool.GitControl/_gen/BatchFileCreator.cs
@@ -2,11 +2,10 @@
 // <author>Christian Sack</author>
 // <email>christian@sack.at</email>
 // <website>christian.sack.at</website>
-// <date>2016-06-01</date>
+// <date>2016-06-02</date>
 
 using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -15,6 +14,7 @@ using System.Text.RegularExpressions;
 using BillingToolDataAccess.sqlcedatabases.billingdatabase._Extensions.enumerations;
 using BillingToolGitControl.codeSamples;
 using CsWpfBase.Ev.Public.Extensions;
+// ReSharper disable InconsistentNaming
 
 
 
@@ -36,26 +36,30 @@ namespace BillingToolGitControl._gen
 													"Frühgeschichte zugerechnet Somit wird eine aber dennoch sehr bedeutsame Definition des Gegenstandes" +
 													" der Geschichtswissenschaft durch die Überlieferung von Texten gegeben";
 		private static List<BillingToolStarter.Posten> _manyPostensArray;
-		private static BillingToolStarter _starter = new BillingToolStarter("", $"{Utils.Build.Version.Name}-SCRIPT");
+		private static readonly BillingToolStarter _starter = new BillingToolStarter("", $"{Utils.Build.Version.Name}-SCRIPT");
 		private static List<BillingToolStarter.Posten> _defaultPostensArray;
 
-		private static List<int> Steuersätze = new List<int>() {20, 13, 10, 0, 19};
+		private static readonly List<int> Steuersätze = new List<int>() {20, 13, 10, 0, 19};
+		private static string TargetFolder;
 		private static List<BillingToolStarter.Posten> ManyPostensArray
 		{
-			get { return _manyPostensArray ?? (_manyPostensArray = GetSeparatedWords(2).Select(x => new BillingToolStarter.Posten(x, (decimal)Rand.Next(100, 999999) / (decimal)100, 20, Rand.Next(1, 10))).ToList()); }
+			get { return _manyPostensArray ?? (_manyPostensArray = GetSeparatedWords(2).Select(x => new BillingToolStarter.Posten(x, Rand.Next(100, 999999)/(decimal) 100, 20, Rand.Next(1, 10))).ToList()); }
 		}
 		private static List<BillingToolStarter.Posten> DefaultPostensArray
 		{
-			get { return _defaultPostensArray ?? (_defaultPostensArray = new List<BillingToolStarter.Posten>
+			get
 			{
-				new BillingToolStarter.Posten("Kurs A", (decimal)Rand.Next(100, 99999) / (decimal)100, Steuersätze[Rand.Next(0, Steuersätze.Count)], Rand.Next(1,5)),
-				new BillingToolStarter.Posten("Kurs B", (decimal)Rand.Next(100, 99999) / (decimal)100, Steuersätze[Rand.Next(0, Steuersätze.Count)], Rand.Next(1,5)),
-				new BillingToolStarter.Posten("Gebühr Kurs C", (decimal)Rand.Next(100, 99999) / (decimal)100, Steuersätze[Rand.Next(0, Steuersätze.Count)], Rand.Next(1,5)),
-				new BillingToolStarter.Posten("Reinigungsaufschlag", (decimal)Rand.Next(100, 99999) / (decimal)100, Steuersätze[Rand.Next(0, Steuersätze.Count)], Rand.Next(1,5)),
-				new BillingToolStarter.Posten("Nächtigungsgebühr", (decimal)Rand.Next(100, 99999) / (decimal)100, Steuersätze[Rand.Next(0, Steuersätze.Count)], Rand.Next(1,5)),
-			}); }
+				return _defaultPostensArray ?? (_defaultPostensArray = new List<BillingToolStarter.Posten>
+				{
+					new BillingToolStarter.Posten("Kurs A", Rand.Next(100, 99999)/(decimal) 100, Steuersätze[Rand.Next(0, Steuersätze.Count)], Rand.Next(1, 5)),
+					new BillingToolStarter.Posten("Kurs B", Rand.Next(100, 99999)/(decimal) 100, Steuersätze[Rand.Next(0, Steuersätze.Count)], Rand.Next(1, 5)),
+					new BillingToolStarter.Posten("Gebühr Kurs C", Rand.Next(100, 99999)/(decimal) 100, Steuersätze[Rand.Next(0, Steuersätze.Count)], Rand.Next(1, 5)),
+					new BillingToolStarter.Posten("Reinigungsaufschlag", Rand.Next(100, 99999)/(decimal) 100, Steuersätze[Rand.Next(0, Steuersätze.Count)], Rand.Next(1, 5)),
+					new BillingToolStarter.Posten("Nächtigungsgebühr", Rand.Next(100, 99999)/(decimal) 100, Steuersätze[Rand.Next(0, Steuersätze.Count)], Rand.Next(1, 5)),
+				});
+			}
 		}
-		private static string TargetFolder;
+
 		public static void CreateBatchFiles(string targetFolder)
 		{
 			TargetFolder = targetFolder;
@@ -67,10 +71,12 @@ namespace BillingToolGitControl._gen
 				B_Mail_DefaultPosten,
 				C_PrintMail_ManyPosten,
 				C_PrintMail_DefaultPosten,
+				D_Mail_FAILURE_ManyPosten,
+				D_Mail_FAILURE_DefaultPosten,
 				_Options,
 				_Viewer,
 				_SilentMonatsBonPrint,
-			}.ForEach(x=>x());
+			}.ForEach(x => x());
 		}
 
 		private static void A_Print_ManyPosten()
@@ -94,6 +100,17 @@ namespace BillingToolGitControl._gen
 			WriteFile(data);
 		}
 
+		private static void D_Mail_FAILURE_ManyPosten()
+		{
+			var data = GetDefaultBelegData("Eine ganze Menge Posten.", "Dieses Skript soll eine ganze Menge an Posten testen.\r\nAllerdings mit invaliden Mail Adressen.", false, false);
+			data.Mails = new List<BillingToolStarter.Mail> {new BillingToolStarter.Mail("failure.at"), new BillingToolStarter.Mail("failure2.at")};
+			data.Postens = ManyPostensArray;
+			WriteFile(data);
+		}
+
+
+
+
 		private static void A_Print_DefaultPosten()
 		{
 			var data = GetDefaultBelegData("Eine ganz normale Rechnung wenn man so will", "Dieses Skript soll eine übliche Rechnung testen.", true, false);
@@ -115,14 +132,24 @@ namespace BillingToolGitControl._gen
 			WriteFile(data);
 		}
 
+		private static void D_Mail_FAILURE_DefaultPosten()
+		{
+			var data = GetDefaultBelegData("Eine ganz normale Rechnung wenn man so will", "Dieses Skript soll eine übliche Rechnung testen.", false, false);
+			data.Mails = new List<BillingToolStarter.Mail> {new BillingToolStarter.Mail("failure.at"), new BillingToolStarter.Mail("failure2.at")};
+			data.Postens = DefaultPostensArray;
+			WriteFile(data);
+		}
+
 		private static void _Options()
 		{
 			WriteFile(_starter.Cmd_Options());
 		}
+
 		private static void _Viewer()
 		{
 			WriteFile(_starter.Cmd_BelegDataViewer());
 		}
+
 		private static void _SilentMonatsBonPrint()
 		{
 			WriteFile(_starter.Cmd_SilentMonatsBonPrint());
@@ -134,10 +161,11 @@ namespace BillingToolGitControl._gen
 		{
 			WriteFile(_starter.Cmd_BelegDataApproval(data), fileName);
 		}
+
 		private static void WriteFile(string argument, [CallerMemberName] string fileName = null)
 		{
-			argument = ("chcp 1252\r\nsetlocal EnableDelayedExpansion\r\nStart ..\\Executeable\\Billingtool.exe " + argument.Replace("\r\n", "^\r\n\r\n"));
-			string targetFilePath = Path.Combine(TargetFolder, Paths.Arc.RelFolder_Startup, fileName + ".bat");
+			argument = "chcp 1252\r\nsetlocal EnableDelayedExpansion\r\nStart ..\\Executeable\\Billingtool.exe " + argument.Replace("\r\n", "^\r\n\r\n");
+			var targetFilePath = Path.Combine(TargetFolder, Paths.Arc.RelFolder_Startup, fileName + ".bat");
 			new FileInfo(targetFilePath).CreateDirectory_IfNotExists();
 			File.WriteAllText(targetFilePath, argument, Encoding.Default); //
 		}
